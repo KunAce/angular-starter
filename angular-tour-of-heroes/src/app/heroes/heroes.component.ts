9f3645857c632f5bd8087b57dafa4d26522f34e4
KunAce <wxkace@gmail.com>
2020-06-22 23:04:16 -0700

    Added a 'Dashboard' view, the navigation feature between 'Heroes' and 'Dashboard' views, and the feature of detail view when clicking on name

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
 import {HeroService} from "../hero.service";
 import { MessageService } from "../message.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-heroes',
@@ -12,24 +13,16 @@ import { MessageService } from "../message.service";
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id = ${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-
 
 }
