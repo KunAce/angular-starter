0a561f810591781f2c77b40cf66a9628d52ce11a
KunAce <wxkace@gmail.com>
2020-06-28 23:47:38 -0700

    Applied attribute directive and responded to user events

--- displaying-data/src/app/app.component.ts
+++ displaying-data/src/app/app.component.ts
@@ -3,24 +3,25 @@ import {Hero} from "./hero";
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>{{title}}</h1>
-    <h2>My favorite hero is: {{myHero.name }}</h2>
-    <p>Heroes:</p>
-    <ul>
-      <li *ngFor="let hero of heroes">
-        {{ hero.name }}
-      </li>
-    </ul>
-    <p *ngIf="heroes.length > 3 ">There are many heroes!</p>
-    <app-click-me></app-click-me>
-    <app-keyup></app-keyup>
-    <app-loop-back></app-loop-back>
-    <app-keyup2></app-keyup2>
-    <app-keyup3></app-keyup3>
-    <app-keyup4></app-keyup4>
-    <app-little-tour></app-little-tour>
-  `
+  templateUrl: './app.component.html'
+  // template: `
+  // <h1>{{title}}</h1>
+  //   <h2>My favorite hero is: {{myHero.name }}</h2>
+  //   <p>Heroes:</p>
+  //   <ul>
+  //     <li *ngFor="let hero of heroes">
+  //       {{ hero.name }}
+  //     </li>
+  //   </ul>
+  //   <p *ngIf="heroes.length > 3 ">There are many heroes!</p>
+  //   <app-click-me></app-click-me>
+  //   <app-keyup></app-keyup>
+  //   <app-loop-back></app-loop-back>
+  //   <app-keyup2></app-keyup2>
+  //   <app-keyup3></app-keyup3>
+  //   <app-keyup4></app-keyup4>
+  //   <app-little-tour></app-little-tour>
+  // `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -31,4 +32,5 @@ export class AppComponent {
     new Hero(20, 'Tornado')
   ];
   myHero = this.heroes[0];
+  color: string;
 }
--- displaying-data/src/app/highlight.directive.ts
+++ displaying-data/src/app/highlight.directive.ts
@@ -1,12 +1,27 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor="yellow";
+  constructor(private el: ElementRef) {}
+
+  @Input() defaultColor: string;
+
+  @Input('appHighlight') highlightColor: string;
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor= color;
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || this.defaultColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
   }
 
 }
+
