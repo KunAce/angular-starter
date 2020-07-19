5b5553a6880fb82674da22eb17a1f95c3aee3955
KunAce <wxkace@gmail.com>
2020-07-18 19:22:34 -0700

    Added error handling and retry for 'config' service

--- angular-tour-of-heroes/src/app/config/config.service.ts
+++ angular-tour-of-heroes/src/app/config/config.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
 
-import { Observable } from "rxjs";
+import {Observable, throwError} from "rxjs";
+import {catchError, retry} from "rxjs/operators";
 
 export interface Config {
   heroesUrl: string;
@@ -15,7 +16,10 @@ export class ConfigService {
   constructor(private http: HttpClient) { }
 
   getConfig() {
-    return this.http.get<Config>(this.configUrl);
+    return this.http.get<Config>(this.configUrl).
+      pipe(
+        retry(3), // retry a falied request up to 3 times
+      catchError(this.handleError)); // then handle the error
   }
 
   getConfig_1() {
@@ -27,12 +31,29 @@ export class ConfigService {
     return this.http.get<Config>(this.configUrl);
   }
 
-
-
   getConfigResponse(): Observable<HttpResponse<Config>> {
     return this.http.get<Config>(
       this.configUrl, {observe:'response'}
     );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occured. Handle it accodingly.
+      console.error('An error occurred:',error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return and observable with a user-facing error message
+    return throwError(
+      'Something bad happened. Please try again later.'
+    );
+  }
+
+
 
 }
