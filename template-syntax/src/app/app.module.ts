dbd16cc001420b4705c32f42fddfd9891f41a297
KunAce <wxkace@gmail.com>
2020-08-15 22:47:48 -0700

    Added simple two-way binding

--- template-syntax/src/app/app.module.ts
+++ template-syntax/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BigHeroDetailComponent, HeroDetailComponent} from './hero-detail.component';
 import { ClickDirective } from './click.directive';
+import { SizerComponent } from './sizer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     ClickDirective,
-    BigHeroDetailComponent
+    BigHeroDetailComponent,
+    SizerComponent
   ],
   imports: [
     BrowserModule
