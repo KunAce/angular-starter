cf538e84a6e1b99f7a64cbae493ef0c6dc011c23
KunAce <wxkace@gmail.com>
2020-09-03 23:40:10 -0700

    Added components with intercepting input property changes with a setter

--- component-interaction/src/app/app.module.ts
+++ component-interaction/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroChildComponent } from './hero-child.component';
 import { HeroParentComponent } from './hero-parent.component';
+import { NameParentComponent } from './name-parent.component';
+import { NameChildComponent } from './name-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroChildComponent,
-    HeroParentComponent
+    HeroParentComponent,
+    NameParentComponent,
+    NameChildComponent
   ],
   imports: [
     BrowserModule
