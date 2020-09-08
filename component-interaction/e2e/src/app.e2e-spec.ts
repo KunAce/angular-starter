90461fcdc5f9fddd34382ac0912048c1dceecdbe
KunAce <wxkace@gmail.com>
2020-09-07 23:30:54 -0700

    Added test cases

---
+++ component-interaction/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,165 @@
+import { AppPage } from './app.po';
+import {browser, by, element, logging} from 'protractor';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('component-interaction app is running!');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+
+  let _heroNames = ['Dr IQ', 'Magneta', 'Bombasto'];
+  let _masterName = 'Master';
+
+  it('should pass properties to children properly', function () {
+    let parent = element.all(by.tagName('app-hero-parent')).get(0);
+    let heroes = parent.all(by.tagName('app-hero-child'));
+
+    for (let i = 0; i < _heroNames.length; i++) {
+      let childTitle = heroes.get(i).element(by.tagName('h3')).getText();
+      let childDetail = heroes.get(i).element(by.tagName('p')).getText();
+      expect(childTitle).toEqual(_heroNames[i] + ' says:');
+      expect(childDetail).toContain(_masterName);
+    }
+  });
+
+
+  it('should display trimmed, non-empty names', function () {
+    let _nonEmptyNameIndex = 0;
+    let _nonEmptyName = '"Dr IQ"';
+    let parent = element.all(by.tagName('app-name-parent')).get(0);
+    let hero = parent.all(by.tagName('app-name-child')).get(_nonEmptyNameIndex);
+
+    let displayName = hero.element(by.tagName('h3')).getText();
+    expect(displayName).toEqual(_nonEmptyName);
+  });
+
+  it('should replace empty name with default name', function () {
+    let _emptyNameIndex = 1;
+    let _defaultName = '"<no name set>"';
+    let parent = element.all(by.tagName('app-name-parent')).get(0);
+    let hero = parent.all(by.tagName('app-name-child')).get(_emptyNameIndex);
+
+    let displayName = hero.element(by.tagName('h3')).getText();
+    expect(displayName).toEqual(_defaultName);
+  });
+
+
+  // Test must all execute in this exact order
+  it('should set expected initial values', function () {
+    let actual = getActual();
+
+    let initialLabel = 'Version 1.23';
+    let initialLog = 'Initial value of major set to 1, Initial value of minor set to 23';
+
+    expect(actual.label).toBe(initialLabel);
+    expect(actual.count).toBe(1);
+    expect(actual.logs.get(0).getText()).toBe(initialLog);
+  });
+
+  it('should set expected values after clicking \'Minor\' twice', function () {
+    let repoTag = element(by.tagName('app-version-parent'));
+    let newMinorButton = repoTag.all(by.tagName('button')).get(0);
+
+    newMinorButton.click().then(function() {
+      newMinorButton.click().then(function() {
+        let actual = getActual();
+
+        let labelAfter2Minor = 'Version 1.25';
+        let logAfter2Minor = 'minor changed from 24 to 25';
+
+        expect(actual.label).toBe(labelAfter2Minor);
+        expect(actual.count).toBe(3);
+        expect(actual.logs.get(2).getText()).toBe(logAfter2Minor);
+      });
+    });
+  });
+
+  it('should set expected values after clicking \'Major\' once', function () {
+    let repoTag = element(by.tagName('app-version-parent'));
+    let newMajorButton = repoTag.all(by.tagName('button')).get(1);
+
+    newMajorButton.click().then(function() {
+      let actual = getActual();
+
+      let labelAfterMajor = 'Version 2.0';
+      let logAfterMajor = 'major changed from 1 to 2, minor changed from 25 to 0';
+
+      expect(actual.label).toBe(labelAfterMajor);
+      expect(actual.count).toBe(4);
+      expect(actual.logs.get(3).getText()).toBe(logAfterMajor);
+    });
+  });
+
+  function getActual() {
+    let versionTag = element(by.tagName('app-version-child'));
+    let label = versionTag.element(by.tagName('h3')).getText();
+    let ul = versionTag.element((by.tagName('ul')));
+    let logs = ul.all(by.tagName('li'));
+
+    return {
+      label: label,
+      logs: logs,
+      count: logs.count()
+    };
+  }
+
+
+  it('should not emit the event initially', function () {
+    let voteLabel = element(by.tagName('app-vote-taker'))
+      .element(by.tagName('h3')).getText();
+    expect(voteLabel).toBe('Agree: 0, Disagree: 0');
+  });
+
+  it('should process Agree vote', function () {
+    let agreeButton1 = element.all(by.tagName('app-voter')).get(0)
+      .all(by.tagName('button')).get(0);
+    agreeButton1.click().then(function() {
+      let voteLabel = element(by.tagName('app-vote-taker'))
+        .element(by.tagName('h3')).getText();
+      expect(voteLabel).toBe('Agree: 1, Disagree: 0');
+    });
+  });
+
+  it('should process Disagree vote', function () {
+    let agreeButton1 = element.all(by.tagName('app-voter')).get(1)
+      .all(by.tagName('button')).get(1);
+    agreeButton1.click().then(function() {
+      let voteLabel = element(by.tagName('app-vote-taker'))
+        .element(by.tagName('h3')).getText();
+      expect(voteLabel).toBe('Agree: 1, Disagree: 1');
+    });
+  });
+
+
+  it('timer and parent seconds should match', function () {
+    let parent = element(by.tagName(parentTag));
+    let message = parent.element(by.tagName('app-countdown-timer')).getText();
+    browser.sleep(10); // give `seconds` a chance to catchup with `message`
+    let seconds = parent.element(by.className('seconds')).getText();
+    expect(message).toContain(seconds);
+  });
+
+  it('should stop the countdown', function () {
+    let parent = element(by.tagName(parentTag));
+    let stopButton = parent.all(by.tagName('button')).get(1);
+
+    stopButton.click().then(function() {
+      let message = parent.element(by.tagName('app-countdown-timer')).getText();
+      expect(message).toContain('Holding');
+    });
+  });
+});
