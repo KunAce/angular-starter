5a1def8ff9cc29e2c3375694fd3ae84acb23b32c
KunAce <wxkace@gmail.com>
2020-09-20 20:57:41 -0700

    Added 'crisis-center' module

--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -4,17 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CrisisCenterComponent } from './crisis-center/crisis-center/crisis-center.component';
+import { CrisisCenterHomeComponent } from './crisis-center/crisis-center-home/crisis-center-home.component';
+import { CrisisDetailComponent } from './crisis-center/crisis-detail/crisis-detail.component';
+import { CrisisListComponent } from './crisis-center/crisis-list/crisis-list.component';
+import {Router} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {HeroesModule} from "./heroes/heroes.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+  }
+}
